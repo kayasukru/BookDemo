7195a31a569b651479913ffbd9c44a51bd53dd2f
Sukru Kaya <kayasukru@gmail.com>
2024-08-06 14:04:37 +0300

    Add project files.

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using BookDemo.Models;
+
+namespace BookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book(){Id=1,Title="Yalan", Price=150},
+                new Book(){Id=2, Title="Gerçek", Price=200},
+                new Book(){Id=3, Title="Hayal", Price=250}
+            };
+        }
+    }
+}
